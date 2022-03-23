a99a01be7304b3d0d7be51a7e33b60ecd0418fb9
this <1341482598@qq.com>
2022-03-23 14:10:12 +0800

    project init, preparation

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,11 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: [["component",
-    {
-      "libraryName": "mint-ui",
-      "style": true
-    }
-  ]]
+  plugins: [
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']
+  ]
 }
